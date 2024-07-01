1424e3a63883852ccf2943da1711e17359b32930
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-07-02 07:01:29 +0800

    Final LiquidityMining Test

--- test/LiquidityMining.t.sol
+++ test/LiquidityMining.t.sol
@@ -27,6 +27,7 @@ contract LiquidityMiningTest is Test, Deployers, GasSnapshot {
     using StateLibrary for IPoolManager;
 
     uint256 constant Q32 = 1 << 32;
+    uint256 rate;
 
     LiquidityMiningHook hook;
     PoolId poolId;
@@ -98,6 +99,205 @@ contract LiquidityMiningTest is Test, Deployers, GasSnapshot {
         assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, -1200), 0, "check34");
         assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, 3000), perLiquidity(100 seconds), "check35");
         assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, 3000), perLiquidity(100 seconds), "check36");
+
+        advanceTime(150 seconds);
+
+        assertEq(hook.getSecondsInside(poolId, -1200, 1200), 100 seconds, "check41");
+        assertEq(hook.getSecondsInside(poolId, 1200, 3000), 150 seconds, "check42");
+        assertEq(hook.getSecondsInside(poolId, -3000, -1500), 0, "check43");
+        assertEq(hook.getSecondsInside(poolId, -1500, -1200), 0, "check44");
+        assertEq(hook.getSecondsInside(poolId, -1500, 3000), 250 seconds, "check45");
+        assertEq(hook.getSecondsInside(poolId, -3000, 3000), 250 seconds, "check46");
+
+        swap2(key, true, 0.17 ether, ZERO_BYTES);
+        assertEq(currentTick(), -1780); // <====== current tick is updated by swap
+
+        assertEq(hook.getSecondsInside(poolId, -1200, 1200), 100 seconds, "check51");
+        assertEq(hook.getSecondsInside(poolId, 1200, 3000), 150 seconds, "check52");
+        assertEq(hook.getSecondsInside(poolId, -3000, -1500), 0, "check53");
+        assertEq(hook.getSecondsInside(poolId, -1500, -1200), 0, "check54");
+        assertEq(hook.getSecondsInside(poolId, -1500, 3000), 250 seconds, "check55");
+        assertEq(hook.getSecondsInside(poolId, -3000, 3000), 250 seconds, "check56");
+
+        advanceTime(25 seconds);
+
+        assertEq(hook.getSecondsInside(poolId, -1200, 1200), 100 seconds, "check61");
+        assertEq(hook.getSecondsInside(poolId, 1200, 3000), 150 seconds, "check62");
+        assertEq(hook.getSecondsInside(poolId, -3000, -1500), 25 seconds, "check63");
+        assertEq(hook.getSecondsInside(poolId, -1500, -1200), 0, "check64");
+        assertEq(hook.getSecondsInside(poolId, -1500, 3000), 250 seconds, "check65");
+        assertEq(hook.getSecondsInside(poolId, -3000, 3000), 275 seconds, "check66");
+    }
+
+    function testSecondsPerLiquidityInside() public {
+        //  Liquidity Distribution
+        //
+        //                                     price
+        //  -3000  ----- -1500  --- -1200 -----  0  -----  1200  -----  3000
+        //                             ======================
+        //                                                   ==============
+        //    ===============
+        //                     < gap >
+        //
+        addLiquidity({tickLower: -1200, tickUpper: 1200});
+        addLiquidity({tickLower: 1200, tickUpper: 3000});
+        addLiquidity({tickLower: -3000, tickUpper: -1500});
+
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1200, 1200), 0, "check11");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, 1200, 3000), 0, "check12");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, -1500), 0, "check13");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, -1200), 0, "check14");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, 3000), 0, "check15");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, 3000), 0, "check16");
+
+        advanceTime(100 seconds);
+
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1200, 1200), perLiquidity(100 seconds), "check21");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, 1200, 3000), 0, "check22");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, -1500), 0, "check23");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, -1200), 0, "check24");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, 3000), perLiquidity(100 seconds), "check25");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, 3000), perLiquidity(100 seconds), "check26");
+
+        swap2(key, false, 0.09 ether, ZERO_BYTES);
+        assertEq(currentTick(), 1886); // <===== current tick is updated by swap
+
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1200, 1200), perLiquidity(100 seconds), "check31");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, 1200, 3000), 0, "check32");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, -1500), 0, "check33");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, -1200), 0, "check34");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, 3000), perLiquidity(100 seconds), "check35");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, 3000), perLiquidity(100 seconds), "check36");
+
+        advanceTime(150 seconds);
+
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1200, 1200), perLiquidity(100 seconds), "check41");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, 1200, 3000), perLiquidity(150 seconds), "check42");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, -1500), 0, "check43");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, -1200), 0, "check44");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, 3000), perLiquidity(250 seconds), "check45");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, 3000), perLiquidity(250 seconds), "check46");
+
+        swap2(key, true, 0.17 ether, ZERO_BYTES);
+        assertEq(currentTick(), -1780); // <===== current tick is updated by swap
+
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1200, 1200), perLiquidity(100 seconds), "check51");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, 1200, 3000), perLiquidity(150 seconds), "check52");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, -1500), 0, "check53");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, -1200), 0, "check54");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, 3000), perLiquidity(250 seconds), "check55");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, 3000), perLiquidity(250 seconds), "check56");
+
+        advanceTime(25 seconds);
+
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1200, 1200), perLiquidity(100 seconds), "check61");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, 1200, 3000), perLiquidity(150 seconds), "check62");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -3000, -1500), perLiquidity(25 seconds), "check63");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, -1200), 0, "check64");
+        assertEq(hook.getSecondsPerLiquidityInsideX128(poolId, -1500, 3000), perLiquidity(250 seconds), "check65");
+        assertEq(perLiquidity(275 seconds), 93577650903258077452428);
+        assertEq(
+            hook.getSecondsPerLiquidityInsideX128(poolId, -3000, 3000),
+            perLiquidity(25) + perLiquidity(100) + perLiquidity(150),
+            "check66"
+        );
+    }
+
+    function test_SingleLp_LiquidityPoints() external {
+        PositionRef memory p1 = addLiquidity(-1200, 1200);
+
+        assertEq(getLiquidityPoints(p1), 0, "check1");
+        advanceTime(100 seconds);
+        assertEq(getLiquidityPoints(p1), (100 seconds << 32) - 1, "check2");
+    }
+
+    function test_TwoLP_LiquidityPoints_1() external {
+        PositionRef memory p1 = addLiquidity(1e18, -1200, 1200, keccak256("1"));
+        PositionRef memory p2 = addLiquidity(3e18, -1200, 1200, keccak256("2"));
+
+        assertEq(getLiquidityPoints(p1), 0, "check11");
+        assertEq(getLiquidityPoints(p2), 0, "check12");
+
+        advanceTime(100 seconds);
+
+        assertEq(getLiquidityPoints(p1), (25 seconds << 32) - 1, "check21");
+        assertEq(getLiquidityPoints(p2), (75 seconds << 32) - 1, "check22");
+    }
+
+    function test_TwoLP_LiqudityPoints_2() external {
+        PositionRef memory p1 = addLiquidity(1e18, -1200, 1200, keccak256("1"));
+        PositionRef memory p2 = addLiquidity(3e18, -1200, 1200, keccak256("2"));
+
+        assertEq(getLiquidityPoints(p1), 0, "check11");
+        assertEq(getLiquidityPoints(p2), 0, "check12");
+
+        advanceTime(100 seconds);
+
+        assertEq(getLiquidityPoints(p1), (25 seconds << 32) - 1, "check21");
+        assertEq(getLiquidityPoints(p2), (75 seconds << 32) - 1, "check22");
+
+        PositionRef memory p3 = addLiquidity(4e18, -1200, 1200, keccak256("3"));
+
+        assertEq(getLiquidityPoints(p1), (25 seconds << 32) - 1, "check31");
+        assertEq(getLiquidityPoints(p2), (75 seconds << 32) - 1, "check32");
+        assertEq(getLiquidityPoints(p3), 0, "check33");
+
+        advanceTime(200 seconds);
+
+        assertEq(getLiquidityPoints(p1), (25 seconds << 32) - 1 + (25 seconds << 32) - 1, "check41");
+        assertEq(getLiquidityPoints(p2), (75 seconds << 32) - 1 + (75 seconds << 32) - 1, "check42");
+        assertEq(getLiquidityPoints(p3), (100 seconds << 32) - 1, "check43");
+    }
+
+    function test_TwoLP_Streams_1() external {
+        PositionRef memory p1 = addLiquidity(1e18, -1200, 1200, keccak256("1"));
+        PositionRef memory p2 = addLiquidity(3e18, -1200, 1200, keccak256("2"));
+
+        createStream(-1200, 1200, rate = 1 ether, 100 seconds);
+
+        assertEq(getStreams(p1), 0, "check11");
+        assertEq(getStreams(p2), 0, "check12");
+
+        advanceTime(20 seconds);
+
+        assertEqWithError({actual: getStreams(p1), expected: 5 ether, maxError: Q32, m: "check21"});
+        assertEqWithError({actual: getStreams(p2), expected: 15 ether, maxError: Q32, m: "check22"});
+
+        PositionRef memory p3 = addLiquidity(4e18, -1200, 1200, keccak256("3"));
+
+        assertEqWithError({actual: getStreams(p1), expected: 5 ether, maxError: Q32, m: "check31"});
+        assertEqWithError({actual: getStreams(p2), expected: 15 ether, maxError: Q32, m: "check32"});
+        assertEq(getStreams(p3), 0, "check33");
+
+        advanceTime(20 seconds);
+
+        assertEqWithError({actual: getStreams(p1), expected: 5 ether + 2.5 ether, maxError: Q32, m: "check41"});
+        assertEqWithError({actual: getStreams(p2), expected: 15 ether + 7.5 ether, maxError: Q32, m: "check42"});
+        assertEqWithError({actual: getStreams(p3), expected: 10 ether, maxError: Q32, m: "check43"});
+
+        withdrawStreams(p1, address(1111));
+        assertEqWithError({
+            actual: streamToken.balanceOf(address(1111)),
+            expected: 7.5 ether,
+            maxError: Q32,
+            m: "check51"
+        });
+
+        withdrawStreams(p2, address(2222));
+        assertEqWithError({
+            actual: streamToken.balanceOf(address(2222)),
+            expected: 22.5 ether,
+            maxError: Q32,
+            m: "check52"
+        });
+
+        withdrawStreams(p3, address(3333));
+        assertEqWithError({
+            actual: streamToken.balanceOf(address(3333)),
+            expected: 10 ether,
+            maxError: Q32,
+            m: "check53"
+        });
     }
 
     function addLiquidity(int24 tickLower, int24 tickUpper) internal returns (PositionRef memory) {
@@ -149,6 +349,46 @@ contract LiquidityMiningTest is Test, Deployers, GasSnapshot {
         snapStart("swap");
         value = swap(_key, zeroForOne, amountSpecified, hookData);
         snapEnd();
+    }
+
+    function getLiquidityPoints(PositionRef memory p) internal returns (uint256 liquidityPoints) {
+        vm.prank(address(0));
+        (liquidityPoints,,) = hook.getUpdatedPosition(
+            key.toId(), p.owner, p.tickLower, p.tickUpper, p.salt, msg.sender, ERC20(address(0)), 0
+        );
+    }
+
+    function createStream(int24 tickLower, int24 tickUpper, uint256 streamRate, uint48 duration) internal {
+        streamToken.mint(streamRate * duration);
+        streamToken.approve(address(hook), streamRate * duration);
+        hook.createStream(key.toId(), tickLower, tickUpper, streamToken, streamRate, duration);
+    }
+
+    function getStreams(PositionRef memory p) internal returns (uint256 streams) {
+        vm.prank(address(0));
+        (,, streams) = hook.getUpdatedPosition(
+            key.toId(), p.owner, p.tickLower, p.tickUpper, p.salt, address(this), streamToken, rate
+        );
+    }
+
+    function assertEqWithError(uint256 actual, uint256 expected, uint256 maxError, string memory m) internal pure {
+        uint256 diff = actual < expected ? expected - actual : actual - expected;
+        if (diff > maxError) {
+            assertEq(actual, expected, m);
+        }
+    }
+
+    function withdrawStreams(PositionRef memory p, address beneficiary) internal {
+        modifyLiquidityRouter.modifyLiquidity(
+            key,
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: p.tickLower,
+                tickUpper: p.tickUpper,
+                liquidityDelta: 0,
+                salt: p.salt
+            }),
+            abi.encode(address(this), streamToken, rate, beneficiary)
+        );
     }
 }
 
